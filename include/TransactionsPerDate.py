00a427d1ccb927f1fe5f0b867a4fd5a6fd5f7aee
MLobo1997 <miguelobo1997@gmail.com>
2019-03-04 18:43:35 +0000

    it seems to be working

---
+++ include/TransactionsPerDate.py
@@ -0,0 +1,34 @@
+from copy import deepcopy
+
+def sumTuples(tupleA, tupleB):
+    return tuple([a + b for a, b in zip(tupleA, tupleB)])
+
+class TransactionsPerDate():
+
+    def __init__(self):
+        self.transactions = {}
+
+    def insertTransaction(self, user, datetime, amount):
+        if user not in self.transactions:
+            self.transactions[user] = {}
+        if datetime.date() not in self.transactions[user]:
+            self.transactions[user][datetime.date()] = (1, amount)
+        else:
+            user_day_content = self.transactions[user][datetime.date()]
+            new_values = (1, amount)
+            self.transactions[user][datetime.date()] = sumTuples(user_day_content, new_values)
+    
+    def getTransactionAveragesExceptDay(self, user, datetime):
+        user_transaction_measures = deepcopy(self.transactions[user])
+        del user_transaction_measures[datetime.date()]
+        N = len(user_transaction_measures)
+        if N != 0:
+            tuple_sum = (0,0)
+            for _, tran in user_transaction_measures.items():
+                tuple_sum = sumTuples(tuple_sum, tran)
+            return tuple([x / N for x in tuple_sum])
+        else:
+            return (1,188.0)
+
+    def getTransactionsDay(self, user, datetime):
+        return self.transactions[user][datetime.date()]
--- include/UserEvaluator.py
+++ include/UserEvaluator.py
@@ -1,26 +1,57 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-from include.Transactions import Transactions
+from include.TransactionsPerDate import TransactionsPerDate
+from pandas import Timestamp
+
+
+def divTuples(tupleA, tupleB):
+    return tuple([a / b for a, b in zip(tupleA, tupleB)])
+
 
 class UserEvaluator(BaseEstimator, TransformerMixin):
 
-    def __init__(self, col_names, copy=True):
-        self.copy=copy
-        self.col_names=col_names
-        self.transactions=Transactions()
+    def __init__(self, use_card = False, N_attribute_name='daily_transactions_ratio', amount_attribute_name='amount_transactions_ratio', copy=True):
+        self.copy = copy
+        self.transactions = TransactionsPerDate()
+        self.first = True
+        self.N_attribute_name = N_attribute_name
+        self.amount_attribute_name = amount_attribute_name
+        if use_card:
+            self.user_attr='user_id'
+        else:
+            self.user_attr='card_id'
 
-    def fit(self, X, y=None):
-        for row in X.itertuples(index = False):
-            user = row[self.col_names.index('user_id')]
-            id_num = row[self.col_names.index('id')]
-            datetime = row[self.col_names.index('datetime')]
-            amount = row[self.col_names.index('amount')]
+    def createTransactions(self, X):
+        names = list(X)
+        for row in X.itertuples(index=False):
+            user = row[names.index(self.user_attr)]
+            datetime = row[names.index('datetime')]
+            amount = row[names.index('amount')]
             self.transactions.insertTransaction(
                 user,
-                id_num,
                 datetime,
                 amount,
             )
+
+    def fit(self, X, y=None):
+        self.createTransactions(X)
         return self
 
     def transform(self, X):
+        if self.copy:
+            X = X.copy()
+
+        if self.first:
+            self.first = False
+        else:
+            self.createTransactions(X)
+        
+        new_attr = [
+            divTuples(self.transactions.getTransactionsDay(user, datetime),
+                      self.transactions.getTransactionAveragesExceptDay(user, datetime))
+            for user, datetime in zip(X[self.user_attr], X['datetime'])
+        ]
+
+        a, b = zip(*new_attr)
+        X[self.N_attribute_name], X[self.amount_attribute_name] = list(a), list(b)
+
         return X
